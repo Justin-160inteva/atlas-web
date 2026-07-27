f5037c66ac7f7f56a9164085e4c2b7aa3a788a05
Linder <147746986+Justin-160inteva@users.noreply.github.com>
2026-07-27 23:16:59 +0100

    Add COD11 translator offline cache

---
+++ cod11-live-translator/sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'cod11-live-translator-v2';
+const FILES = [
+  './', './index.html', './styles.css', './app.js', './manifest.webmanifest',
+  './icon.svg'
+];
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))),
+    self.clients.claim()
+  ]));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+    return response;
+  }).catch(() => event.request.mode === 'navigate' ? caches.match('./index.html') : undefined)));
+});
